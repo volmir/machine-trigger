395d0992f8ca30a392765c36c8f708a76f58f1f8
Vladimir Prokhnenko <volmir@ukr.net>
2020-11-10 08:51:52 +0200

    Add project files

--- machine_trigger/machine_trigger.ino
+++ machine_trigger/machine_trigger.ino
@@ -1,17 +1,23 @@
 
 
 #define sensorPin 2
+#define motionPin 3
 
 #define relay1Pin 5
 #define relay2Pin 6
 
-boolean sensorFlag = true;
+boolean sensorState = true;
 boolean sensor = false;
 unsigned long sensorTimer;
+
+boolean motionState = false;  
+boolean motionButton = false;
+unsigned long motionTimer;
 
 
 void setup() {
   pinMode(sensorPin, INPUT_PULLUP);
+  pinMode(motionPin, INPUT_PULLUP);
 
   pinMode(relay1Pin, OUTPUT);
   pinMode(relay2Pin, OUTPUT);
@@ -21,22 +27,34 @@ void setup() {
 }
 
 void loop() {
-  sensor = digitalRead(sensorPin);
+  motionButton = digitalRead(motionPin);  
+  if (motionButton && millis() - motionTimer > 250) {
+    motionTimer = millis();
+    motionState = !motionState;
+  }
+
+  if (motionState) {
+    proceedMotion();
+  }
+}
+
+void proceedMotion() {
+  sensor = digitalRead(sensorPin);  
 
   if (sensor && millis() - sensorTimer > 500) {
-    sensorFlag = !sensorFlag;
+    sensorState = !sensorState;
     sensorTimer = millis();
 
-    if (sensorFlag) {
+    if (sensorState) {
       digitalWrite(relay1Pin, LOW);
     } else {
       digitalWrite(relay1Pin, HIGH);
     }
     delay(100);
-    if (sensorFlag) {
+    if (sensorState) {
       digitalWrite(relay2Pin, HIGH);
     } else {
       digitalWrite(relay2Pin, LOW);
     }
-  }
+  }  
 }
